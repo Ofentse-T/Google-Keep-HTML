0d6552558f057af71849ad780197ddfe1cd016bc
Ofentse-T <122441914+Ofentse-T@users.noreply.github.com>
2023-10-27 17:05:08 +0200

    adding notes

--- app.js
+++ app.js
@@ -11,13 +11,52 @@ class App {
         this.notes = [];
 
         this.$activeForm = document.querySelector(".active-form");
-        this.$activeForm.style.display = "block";
-        console.log(this.$activeForm);
+        this.$inactiveForm = document.querySelector(".inactive-form");
+        this.$noteTitle = document.querySelector("#note-title");
+        this.$noteText = document.querySelector("#note-text");
+        this.$notes = document.querySelector(".notes");
+
+        this.addEventListeners();
+
+        this.addEventListeners();
     }
 
+    addEventListeners() {
+        document.body.addEventListener("click", () => {
+            this.handleFormClick(event);
+        })
+    }
+
+    handleFormClick(event) {
+        const isActiveFormClickedOn = this.$activeForm.contains(event.target);
+        const isInActiveFormClickedOn = this.$inactiveForm.contains(event.target);
+
+        if(isInActiveFormClickedOn) {
+            this.openActiveForm();
+        }
+        else if(!isInActiveFormClickedOn && !isActiveFormClickedOn) {
+            this.closeActiveForm();
+        }
+    }
+
+    openActiveForm() {
+            this.$inactiveForm.style.display = "none";
+            this.$activeForm.style.display = "block";
+            this.$noteText.focus();
+    }
+    closeActiveForm() {
+        this.$inactiveForm.style.display = "block";
+        this.$activeForm.style.display = "none";
+        this.$noteText.value = "";
+        this.$noteTitle.value = "";
+}
+
     addNote(id, {title, text}) {
-        const newNote = new Note(id, title, text);
-        this.notes = [...this.notes, newNote];
+        if(text != "") {
+            const newNote = new Note(cuid(), title, text);
+            this.notes = [...this.notes, newNote];
+            this.displayNotes();
+        }
     }
 
     editNote(id, {title, text }) {
@@ -35,13 +74,48 @@ class App {
     }
 
     displayNotes() {
-        this.notes.map((note) =>
-            console.log(`
-        ID: ${note.id}
-        Title: ${note.title}
-        Text: ${note.text}        
-        `)
-            );
+        this.$notes.innerHTML = this.notes.map((note) =>
+        `
+    <div class="note" id="${note.id}">
+        <span class="material-symbols-outlined">check_circle</span>
+        <div class="title">${note.title}</div>
+        <div class="text">${note.text}</div>
+        <div class="note-footer"><div class="tooltip">
+            <span class="material-symbols-outlined hover small-icon">add_alert</span>
+            <span class="tooltip-text">Remind me</span>
+        </div>
+        <div class="tooltip">
+            <span class="material-symbols-outlined hover small-icon">person_add</span>
+            <span class="tooltip-text">Collaborator</span>
+        </div>
+        <div class="tooltip">
+            <span class="material-symbols-outlined hover small-icon">palette</span>
+            <span class="tooltip-text">Change colour</span>
+        </div>
+        <div class="tooltip">
+            <span class="material-symbols-outlined hover small-icon">image</span>
+            <span class="tooltip-text">Add image</span>
+        </div>
+        <div class="tooltip">
+            <span class="material-symbols-outlined hover small-icon">archive</span>
+            <span class="tooltip-text">Archive</span>
+        </div>
+        <div class="tooltip">
+            <span class="material-symbols-outlined hover small-icon">more_vert</span>
+            <span class="tooltip-text">New Image</span>
+        </div>
+        <div class="tooltip">
+            <span class="material-symbols-outlined hover small-icon">undo</span>
+            <span class="tooltip-text">Undo</span>
+        </div>
+        <div class="tooltip">
+            <span class="material-symbols-outlined hover small-icon">redo</span>
+            <span class="tooltip-text">Redo</span>
+        </div>
+        </div>
+    </div>        
+        `
+        ).join(" ");
     }
 }
 
